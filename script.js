6a3499bad3b9d7a4ae1f468e3e6696a58fea9eb9
emma <egujral26@aoiths.org>
2026-03-15 14:53:55 -0400

    added all variables

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+            event.preventDefault();
 
- document.querySelector("form-control").value = inputValues;
+            var firstName = document.querySelectorAll(".form-control")[0].value;
+            var lastName = document.querySelectorAll(".form-control")[1].value;
+            var email = document.querySelectorAll(".form-control")[2].value;
+            var password = document.querySelectorAll(".form-control")[3].value;
 
+            var country = document.querySelector(".form-select").value;
 
- 
+            var accountType = document.querySelector('input[name ="accountType"]:checked').nextElementSibling.textContent;
+
+            var about = document.querySelector("textarea").value;
+            
+
+    });
 
 });
